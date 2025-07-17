f4cb2a1debcf5e568f04faa7f9cc27d96e07a995
haroldiaz <harolfz@gmail.com>
2025-07-16 22:22:32 -0500

    fix:add page login

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+
+import '../Styles/Login/login.css'
+export default  function Login() {
+
+    const navigate = useNavigate();
+
+    const handleMenuPrincipal = () => {
+			 navigate("/");
+
+    }
+    return (
+        <div>
+            <div className="container-form-login">
+                <form action="">
+                    <h1>Login</h1>
+                     <TextField
+                    label="Nombre"
+                    variant="outlined"
+                    fullWidth
+                    />
+                    <TextField
+                    label="Contrasenia"
+                    variant="outlined"
+                    fullWidth
+                    />
+                    <Button 
+                    variant="contained" 
+                    type="submit"
+                    onClick={handleMenuPrincipal}>
+                        Iniciar Sesion
+                    </Button>
+                </form>
+                  
+            </div>
+
+        </div>
+    );
+};
